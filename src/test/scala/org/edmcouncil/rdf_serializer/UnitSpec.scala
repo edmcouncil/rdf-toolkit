2684d930fbc8926dea1914bae036986b9aebb6af
Jacobus Geluk <jacobus.geluk@gmail.com>
2014-12-08 12:54:43 -0500

    RDFSER-7 Minor Changes

---
+++ src/test/scala/org/edmcouncil/rdf_serializer/UnitSpec.scala
@@ -0,0 +1,20 @@
+package org.edmcouncil.rdf_serializer
+
+import java.io.OutputStream
+
+import org.scalatest.{Matchers, WordSpecLike}
+
+abstract class UnitSpec
+  extends WordSpecLike with Matchers {
+
+  def suppressOutput[T](thunk: => T): T = {
+
+    val bitBucket = new OutputStream() {
+      def write(b: Int) {}
+    }
+
+    Console.withOut(bitBucket) {
+      return thunk
+    }
+  }
+}
