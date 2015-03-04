ea1a325126b23f52a0f1e6333a2fcfe50e19b44f
Anthony B. Coates <abcoates@contakt.org>
2015-03-04 21:36:26 +0000

    https://jira.edmcouncil.org/browse/RDFSER-20: Modify Sesame serializer to consistently write output in UTF-8

--- src/test/scala/org/edmcouncil/rdf_serializer/SesameSortedTurtleWriterSpec.scala
+++ src/test/scala/org/edmcouncil/rdf_serializer/SesameSortedTurtleWriterSpec.scala
@@ -19,7 +19,9 @@ import org.scalatest.{Matchers, FlatSpec}
 /**
  * ScalaTest tests for the SortedTurtleWriter and SortedTurtleWriterFactory.
  */
-class SesameSortedTurtleWriterSpec extends FlatSpec with Matchers with OutputSuppressor {
+class SesameSortedTurtleWriterSpec extends FlatSpec with Matchers /*with OutputSuppressor*/ {
+
+  private val logger = LoggerFactory.getLogger(classOf[SesameSortedTurtleWriterSpec])
 
   // Set up directories for output files.
   def mkCleanDir(dirPath: String): File = {
@@ -60,6 +62,63 @@ class SesameSortedTurtleWriterSpec extends FlatSpec with Matchers with OutputSup
   def getFileContents(file: File): String = new BufferedSource(new FileInputStream(file)).mkString
 
   def getFileContents(file: File, encoding: String): String = new BufferedSource(new FileInputStream(file))(new Codec(Charset.forName(encoding))).mkString
+
+  def compareFiles(file1: File, file2: File, encoding: String): Boolean = {
+    logger.debug("CompareFiles:")
+    logger.debug(s"   left = ${file1.getAbsolutePath}")
+    logger.debug(s"  right = ${file2.getAbsolutePath}")
+    val source1Lines = new BufferedSource(new FileInputStream(file1))(new Codec(Charset.forName(encoding))).getLines()
+    val source2Lines = new BufferedSource(new FileInputStream(file2))(new Codec(Charset.forName(encoding))).getLines()
+    compareStringIterators(source1Lines, source2Lines)
+    logger.debug("CompareFiles: done")
+    true
+  }
+
+  def compareStringIterators(iter1: Iterator[String], iter2: Iterator[String]): Boolean = {
+    var lineCount = 0
+    while (iter1.hasNext || iter2.hasNext) {
+      lineCount += 1
+      if (!iter2.hasNext) {
+        logger.error(s"left file has more lines than right ($lineCount+): ${iter1.next()}")
+        return false
+      }
+      if (!iter1.hasNext) {
+        logger.error(s"right file has more lines than left ($lineCount+): ${iter2.next()}")
+        return false
+      }
+      val line1 = iter1.next()
+      val line2 = iter2.next()
+      if (!compareStrings(line1, line2, lineCount)) {
+        return false
+      }
+    }
+    true
+  }
+
+  def compareStrings(str1: String, str2: String, lineCount: Int): Boolean = {
+    if ((str1.length >= 1) || (str2.length >= 1)) {
+      var index = 0
+      while ((str1.length > index) || (str2.length > index)) {
+        if (str2.length <= index) {
+          logger.error(s"left line ($lineCount) is longer than (${index+1}+) than right: tail = ${str1.substring(index)}")
+          return false
+        }
+        if (str1.length <= index) {
+          logger.error(s"right line ($lineCount) is longer than (${index+1}+) than left: tail = ${str2.substring(index)}")
+          return false
+        }
+        var leftCh = str1.charAt(index)
+        var rightCh = str2.charAt(index)
+        if (leftCh != rightCh) {
+          logger.error(s"char mismatch at $lineCount:${index+1} => $leftCh [#${leftCh.toShort}] != $rightCh [#${rightCh.toShort}]")
+        }
+        index += 1
+      }
+      true
+    } else {
+      true
+    }
+  }
 
   "A SortedTurtleWriterFactory" should "be able to create a SortedTurtleWriter" in {
     val outWriter = new OutputStreamWriter(System.out)
@@ -321,9 +380,7 @@ class SesameSortedTurtleWriterSpec extends FlatSpec with Matchers with OutputSup
       fileCount += 1
       val file2 = new File(outputDir2, file1 getName)
       assert(file2 exists, s"file missing in outputDir2: ${file2.getName}")
-      val contents1 = getFileContents(file1, "UTF-8")
-      val contents2 = getFileContents(file2, "UTF-8")
-      assert(contents1 == contents2, s"match failed for file: ${file1 getAbsolutePath}")
+      assert(compareFiles(file1, file2, "UTF-8"), s"file mismatch between outputDir1 and outputDir2: ${file1.getName}")
     }
   }
 
@@ -360,9 +417,7 @@ class SesameSortedTurtleWriterSpec extends FlatSpec with Matchers with OutputSup
       fileCount += 1
       val file2 = new File(outputDir2, file1 getName)
       assert(file2 exists, s"file missing in outputDir2: ${file2.getName}")
-      val contents1 = getFileContents(file1, "UTF-8")
-      val contents2 = getFileContents(file2, "UTF-8")
-      assert(contents1 == contents2, s"match failed for file: ${file1 getAbsolutePath}")
+      assert(compareFiles(file1, file2, "UTF-8"), s"file mismatch between outputDir1 and outputDir2: ${file1.getName}")
     }
 
     // Check that the re-serialised Turtle file have the same triple count as the matching raw files
