53dcb25279f078377ee62f229b98173aacc4c9ef
Dean Allemang <dallemang@workingontologist.com>
2018-06-05 11:13:18 -0400

    Revert "[RDFKIT-77] Have now added support for processing a whole directory of files at a time.  However, you can only process the same type of files (e.g. Turtle, RDF/XML) in a single invocation, but you can filter to select those files from within a directory tree."
    This reverts commit b5b5fdec3556727d64c3c253864aac2355265e8f.

--- src/test/scala/org/edmcouncil/rdf_toolkit/SesameSortedTurtleWriterSpec.scala
+++ src/test/scala/org/edmcouncil/rdf_toolkit/SesameSortedTurtleWriterSpec.scala
@@ -20,28 +20,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- */ ; /*
- * The MIT License (MIT)
- * 
- * Copyright (c) 2015 Enterprise Data Management Council
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
  */
 package org.edmcouncil.rdf_toolkit
 
@@ -735,6 +713,39 @@ class SesameSortedTurtleWriterSpec extends FlatSpec with Matchers with SesameSor
     val content2 = getFileContents(outputFile2, "UTF-8")
     val singleIndentLineCount2 = content2.lines.filter(_.matches("^\t\t\\S.*$")).size
     assert(singleIndentLineCount2 >= 1, "double-tab indent has failed")
+  }
+
+  def serializeStandardInputToStandardOutput(outputDir: File, inputFile: File, inputFormat: String, outputFormat: String, outputSuffix: String): Unit = {
+    val originalIn = System in
+    val originalOut = System out
+
+    try {
+      val outputFile = constructTargetFile(inputFile, resourceDir, outputDir, Some(outputSuffix))
+      System setIn (new FileInputStream(inputFile))
+      System setOut (new PrintStream(new FileOutputStream(outputFile)))
+
+      SesameRdfFormatter run Array[String](
+        "-sfmt", inputFormat,
+        "-tfmt", outputFormat
+      )
+
+      assert(outputFile exists, s"file missing in outputDir: ${outputFile.getAbsolutePath}")
+      assert(compareFiles(inputFile, outputFile, "UTF-8"), s"file mismatch between inputFile and outputFile: ${inputFile.getName} | ${outputFile.getName}")
+    } catch {
+      case t: Throwable ⇒ throw t
+    } finally {
+      System setIn originalIn
+      System setOut originalOut
+    }
+  }
+
+  it should "be able to use the standard input and output" in {
+    val inputFile = new File("src/test/resources/other/topbraid-countries-ontology.ttl")
+    val outputDir = createTempDir(rootOutputDir1, "turtle")
+
+    serializeStandardInputToStandardOutput(outputDir, inputFile, "turtle", "turtle", ".ttl")
+    serializeStandardInputToStandardOutput(outputDir, inputFile, "turtle", "rdf-xml", ".rdf")
+    serializeStandardInputToStandardOutput(outputDir, inputFile, "turtle", "json-ld", ".jsonld")
   }
 
 }
